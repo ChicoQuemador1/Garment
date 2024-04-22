a545a180b26b6c013abe46bcd97f26ef24b898e6
Mark Denina <146785124+mdenina1@users.noreply.github.com>
2024-04-21 20:32:09 -0400

    Adds new tweaks and Search Bar
    New tweaks:
    - Changes colors of navigation bar
    - Changes font of navigation bar
    
    Search Bar:
    - Adds search bar to home page with experimental data

--- Application/garment/lib/pages/navigation_menu.dart
+++ Application/garment/lib/pages/navigation_menu.dart
@@ -3,6 +3,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import 'store/home_page.dart';
+
 class NavigationMenu extends StatefulWidget {
   const NavigationMenu({super.key});
 
@@ -12,38 +14,75 @@ class NavigationMenu extends StatefulWidget {
 
 class _NavigationMenuState extends State<NavigationMenu> {
   final user = FirebaseAuth.instance.currentUser!;
+  int currentPageIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        destinations: [
-          Container(color: Colors.green),
-          Container(color: Colors.blue),
-          Container(color: Colors.deepPurple),
-          Container(color: Colors.orange),
-        ],
+      bottomNavigationBar: NavigationBarTheme(
+        data: NavigationBarThemeData(
+          labelTextStyle: MaterialStateProperty.resolveWith<TextStyle>(
+            (Set<MaterialState> states) =>
+                states.contains(MaterialState.selected)
+                    ? TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontFamily: 'Sniglet',
+                      )
+                    : TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontFamily: 'Sniglet',
+                      ),
+          ),
+          iconTheme: MaterialStateProperty.resolveWith<IconThemeData>(
+            (Set<MaterialState> states) =>
+                states.contains(MaterialState.selected)
+                    ? IconThemeData(color: Colors.black87)
+                    : IconThemeData(color: Colors.white),
+          ),
+          indicatorColor: Colors.grey[300],
+        ),
+        child: NavigationBar(
+          height: 80,
+          elevation: 0,
+          backgroundColor: Colors.black87,
+          selectedIndex: currentPageIndex,
+          onDestinationSelected: (index) {
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+          destinations: [
+            NavigationDestination(
+              icon: Icon(Icons.home),
+              label: "Home",
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.storefront_rounded),
+              label: "Store",
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.local_mall),
+              label: "Bag",
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.account_circle),
+              label: "Profile",
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: <Widget>[
+          HomePage(),
+          HomePage(),
+          HomePage(),
+          HomePage(),
+        ][currentPageIndex],
       ),
       /*
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("signed in as: ${user.email!}"),
-            // Sign Out Button
-            MaterialButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              color: Colors.black54,
-              child: Text(
-                'Sign Out',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-            )
-          ],
-        ),
+        
       ),
       */
     );
---
+++ Application/garment/lib/pages/store/home_page.dart
@@ -0,0 +1,77 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final user = FirebaseAuth.instance.currentUser!;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              SearchAnchor(
+                  builder: (BuildContext context, SearchController controller) {
+                return SearchBar(
+                  controller: controller,
+                  padding: const MaterialStatePropertyAll<EdgeInsets>(
+                      EdgeInsets.symmetric(horizontal: 16.0)),
+                  onTap: () {
+                    controller.openView();
+                  },
+                  onChanged: (_) {
+                    controller.openView();
+                  },
+                  leading: const Icon(Icons.search),
+                );
+              }, suggestionsBuilder:
+                      (BuildContext context, SearchController controller) {
+                return List<ListTile>.generate(5, (int index) {
+                  final String item = 'item $index';
+                  return ListTile(
+                      title: Text(item),
+                      onTap: () {
+                        setState(() {
+                          controller.closeView(item);
+                        });
+                      });
+                });
+              }),
+              // TEMPORARY Sign out
+              SizedBox(height: 50),
+              Text(
+                "signed in as: ${user.email!}",
+                style: TextStyle(
+                  fontFamily: 'Sniglet',
+                ),
+              ),
+              // Sign Out Button
+              MaterialButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                color: Colors.black54,
+                child: Text(
+                  'Sign Out',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
